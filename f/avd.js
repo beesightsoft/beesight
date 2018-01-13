7075a36152f5e2aaa87b7cad709d1d5ca20320e1
Nhan Cao <nhancv92@gmail.com>
2018-01-13 08:43:39 +0700

    auto start emulator if has one

--- f/avd.js
+++ f/avd.js
@@ -7,12 +7,12 @@ var getEmulatorId = (emulatorIds) => {
     var choices = [];
     for (var i = 0; i < emulatorIds.length; i++) {
         var id = emulatorIds[i];
-        if (id != null && id != undefined && id.length > 0) {
-            choices.push({
-                value: emulatorIds[i],
-                checked: i == 0
-            });
-        }
+        // if (id != null && id != undefined && id.length > 0) {
+        choices.push({
+            value: emulatorIds[i],
+            checked: i == 0
+        });
+        // }
     }
     var questions = [{
         name: 'emulatorId',
@@ -23,18 +23,23 @@ var getEmulatorId = (emulatorIds) => {
     return inquirer.prompt(questions);
 }
 
+const startEmulator = (emId) => {
+    shell.exec("emulator @" + emId + " -timezone Asia/Ho_Chi_Minh", {async: true});
+    process.exit(0);
+}
 const run = () => {
     print.ok("Start Android emulator");
     var emulatorIds = shell.exec('emulator -list-avds', {silent: true}).stdout;
     if (emulatorIds != null && emulatorIds != undefined && emulatorIds.length > 0) {
-        emulatorIds = emulatorIds.split(/\r?\n/);
+        emulatorIds = emulatorIds.split(/\r?\n/).filter(Boolean);
     }
 
-    if (emulatorIds.length > 0) {
+    if (emulatorIds.length > 1) {
         getEmulatorId(emulatorIds).then(data => {
-            shell.exec("emulator @" + data.emulatorId + " -timezone Asia/Ho_Chi_Minh", {async:true});
-            process.exit(0);
-        })
+            startEmulator(data.emulatorId);
+        });
+    } else if (emulatorIds.length == 1) {
+        startEmulator(emulatorIds[0]);
     } else {
         print.error("Don't have any Emulator to start.")
     }
